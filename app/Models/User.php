588b1ff4cbf6940a486082ab93fc9e5c0d8ed877
shashwat <63119158+geforce6t@users.noreply.github.com>
2020-09-08 22:04:28 +0530

    Modified AuthController for login/register route
    -Modified User Model to support Auth.
    -Changed directory path in config/auth -providers.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,22 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Foundation\Auth\User as Authenticatable; 
 
-use Laravel\Passport\HasApiTokens; 
 
-class User extends Model
+class User extends Authenticatable
 {
 
     use Notifiable, HasApiTokens; 
 
     protected $table = 'users';
     protected $hidden = ['email', 'password'];
+    protected $fillable = ['name', 'email', 'password','roll_number','github_handle'];
     public $timestamps = true;
 
     public function projects()
     {
         return $this->belongsToMany('App\Models\Project')->withPivot('role');
     }
-
 }
